1bb0ed95603cc02e9b37246d28134c45acb84970
Juan Sebastian Rodriguez <jsrb48@gmail.com>
2022-09-08 15:12:02 -0500

    Se agregaron los atributos Id, fechaCreacion y fechaActualizacíon en todas las entidades ajustando el Constructor y los Getter y Setter

--- Analisysis_Code/src/main/java/com/tecnomaster/Analisysis_Code/Entities/MovimientoDinero.java
+++ Analisysis_Code/src/main/java/com/tecnomaster/Analisysis_Code/Entities/MovimientoDinero.java
@@ -1,16 +1,23 @@
 package com.tecnomaster.Analisysis_Code.Entities;
 
+import java.util.Date;
+
 public class MovimientoDinero {
     //Atributos
+    private int id;
     private double monto;
-
     private boolean tipoMonto;
-
     private String conceptoMovimiento;
     private Empleado usuario; // Usuario es un objeto de la clase empleado
+    //@Temporal(TemporalType.DATE)
+    //@Column(nullable = false)
+    private Date fechaCreacion;
+    //@Temporal(TemporalType.DATE)
+    //@Column(nullable = false)
+    private Date fechaActualizacion;
 
     //Constructor
-    public MovimientoDinero(double monto, String conceptoMovimiento, Empleado usuario) {
+    public MovimientoDinero(double monto, String conceptoMovimiento, Empleado usuario, Date fechaCreacion, Date fechaActualizacion) {
         this.monto = monto;
         
         if(monto < 0){
@@ -20,6 +27,8 @@ public class MovimientoDinero {
         }
         this.conceptoMovimiento = conceptoMovimiento;
         this.usuario = usuario;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaActualizacion = fechaActualizacion;
     }
 
 
@@ -59,6 +68,22 @@ public class MovimientoDinero {
         return tipoMonto;
     }
 
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+
+    public void setFechaActualizacion(Date fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
     //Metodo toString
 
     @java.lang.Override
@@ -76,4 +101,5 @@ public class MovimientoDinero {
     public void agregarMonto(double nuevoMonto){
         monto += nuevoMonto;
     }
+
 }
