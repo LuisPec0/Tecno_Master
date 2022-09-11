9ebb1d423d989288edb67849d588da76e3a4f4ea
LuisPec0 <luisperezcotes@gmail.com>
2022-09-11 15:32:26 -0500

    Terminación CRUD Perfil

--- Analisysis_Code/src/main/java/com/tecnomaster/Analisysis_Code/Repository/PerfilRepository.java
+++ Analisysis_Code/src/main/java/com/tecnomaster/Analisysis_Code/Repository/PerfilRepository.java
@@ -5,8 +5,13 @@ import com.tecnomaster.Analisysis_Code.Entities.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface PerfilRepository extends JpaRepository<Perfil, String> {
 
+    Optional<Perfil> findById(int id);
+
+    void deleteById(int id);
 }
